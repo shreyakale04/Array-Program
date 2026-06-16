08efec34b82e329860e98c0c19ead4df3c3d1ab4
shreyakale08 <kaleshreya347@gmail.com>
2026-06-17 01:13:10 +0530

    Remove star using Stack and Find Frequency

---
+++ src/basic/Solution.java
@@ -0,0 +1,70 @@
+package basic;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		
+	  String s = "ab**cd*ef*g";
+	  int[]arr = {1,2,3,4,5,6,7,8,9,10};
+	  
+	  Stack<Character>stk = new Stack<>();
+	  
+	  for(char ch : s.toCharArray()){
+		  
+		  if(ch == '*'){
+			  if(!stk.isEmpty()){
+			  stk.pop();
+		  }
+		  }
+		  else{
+		  
+		 stk.push(ch);
+	  }
+		  }
+	  
+	  StringBuilder str = new StringBuilder();
+	  
+	  HashMap<Character, Integer> fq = new HashMap<>();
+	  
+	  for(char c : stk){
+		  
+		  str.append(c);
+		  
+		  fq.put(c, fq.getOrDefault(c, 0) + 1);
+	  }
+	  System.out.println("Final String : " + str);
+	 
+	  String rev = str.reverse().toString();
+	  System.out.println("Reverse String : " + rev);
+	  
+	  int sum = 0;
+	  int max = arr[0];
+	  int min = arr[0];
+	  
+	  for(int num : arr){
+		  
+		  sum += num;
+		  
+		  if(num > max)
+			  max = num;
+		  if(num < min)
+			  min = num;
+	  }
+	  
+	  System.out.println("sum = " + sum);
+	  System.out.println("Maximum = " + max);
+	  System.out.println("Minimum = " + min);
+	  
+	  System.out.println("Character Frequency");
+	  
+	  for(char c : fq.keySet()){
+		  System.out.println(c + "--" + fq.get(c));
+	  }
+	  
+	  
+	}
+
+}
