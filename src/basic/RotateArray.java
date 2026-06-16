2d9d7ed56713020908c8cb2386bf10fea09de6f2
shreyakale08 <kaleshreya347@gmail.com>
2026-06-17 01:11:28 +0530

    Array Rotate

---
+++ src/basic/RotateArray.java
@@ -0,0 +1,22 @@
+package basic;
+
+public class RotateArray {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1, 2, 3, 4, 5};
+		
+		int k = 3;
+		
+		int n = arr.length;
+		int[]result = new int[n];
+		
+		for(int i = 0; i<n; i++){
+			result[i] = arr[(i+k) % n];
+		}
+
+		for(int num : result){
+		System.out.print(num + " ");
+	}
+	}
+}
